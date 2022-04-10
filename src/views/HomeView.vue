5c6af358a99ac0d5bc240bf4c80a632ed9f6b2e9
Lutfi Aldri Permana <12050110415@students.uin-suska.ac.id>
2022-04-10 16:51:02 +0700

    first commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,44 @@
+<template>
+  <!-- Hero Section Start -->
+  <section id="home" class="pt-36">
+    <div class="container">
+      <div class="flex flex-wrap">
+        <div class="w-full self-center px-4 lg:w-1/2">
+          <h1 class="text-left text-base font-semibold text-teal-300 md:text-xl lg:text-2xl">
+            Halo Semua, saya
+            <span class="text-left block font-bold text-4xl">Lutfi Aldri Permana</span>
+          </h1>
+          <h2 class="text-left font-medium text-gray-500 mt-1 text-lg mb-5 lg:text-2xl">
+            Mahasiswa Teknik Informatika UIN Suska Riau
+          </h2>
+          <p class="text-left font-medium text-gray-400 mb-10 leading-relaxed">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste provident earum, maiores
+            eveniet id quas?
+          </p>
+          <!-- <div class="relative h-32 w-32 ...">
+            <div class="absolute top-0 right-0 h-16 w-16 ...">
+              <a href="#"
+                class="text-base font-semibold text-white bg-red-500 py-3 px-8 rounded-full
+                hover:shadow-lg hover:opacity-80 transition duration-300 ease-in-out">ContactMe</a>
+            </div>
+          </div> -->
+        </div>
+        <div class="w-full self-end px-4 lg:w-1/2">
+          <div class="relative mt-10 lg:mt-0 lg:right-0">
+            <img src="../assets/img/1.png" alt="profil" class="max-w-full mx-auto" />
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+  <!-- Hero Section End -->
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: 'HomeView',
+  components: {},
+};
+</script>
